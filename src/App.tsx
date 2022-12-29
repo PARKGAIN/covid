75c461b39f24cc4f3e0b3f182490809e277b3c95
parkgain <pgi082247@gmail.com>
2022-12-30 01:30:45 +0900

    chart.js와 typescript공부용 토이프로젝트

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { JsxElement } from 'typescript'
+import './App.css'
+import Barchart from './components/Barchart';
 
-function App() {
+function App(): JSX.Element {
+  const [cntConfirmations,setCnt] = useState(0)
+  const [rateConfirmations,setRate] =useState(0)
+  const[cntHospitalizations,setCntHos]= useState(0)
+  const [date,setDate] = useState();
+  useEffect(()=>{getData();},[])
+
+  const apiUrl = "http://apis.data.go.kr/1790387/covid19CurrentStatusKorea/covid19CurrentStatusKoreaJason?";
+  const serviceKey = "serviceKey=r5G6Nmk9er5D8IiddKwnNfJdz50cJ4UirOGa55bgfvcu9EXcFvniujfom9fJSIEfBeVnq4cC9EeiOdlWdf2LMQ==";
+ 
+  async function getData() {
+    const response:Response = await fetch(apiUrl + serviceKey);
+    const fetchedData = await response.json();
+    const  covidData = fetchedData.response.result[0];
+    setCnt(covidData.cnt_confirmations);
+    setRate(covidData.rate_confirmations);
+    setCntHos(covidData.cnt_hospitalizations);
+    setDate(covidData.mmddhh);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3 className='text_center'>코로나 현황</h3>
+      <div className='margin_auto'>
+      <h4>데이터조회기준일시 :{date}</h4>
+      <Barchart cntConfirmations={cntConfirmations} rateConfirmations={rateConfirmations} cntHospitalizations={cntHospitalizations}/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Barchart.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  export const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart',
+      },
+    },
+  };
+  
+  const labels = ['일일확진자', '인구10만명당확진','신규입원'];
+  
+interface propTypes{
+    cntConfirmations:number,
+    rateConfirmations:number
+    cntHospitalizations:number
+}
+
+const Barchart = ({cntConfirmations,rateConfirmations,cntHospitalizations}:propTypes) => {
+ const data =  {
+    labels,
+    datasets: [
+        {
+            label: 'data',
+            data: [
+                cntConfirmations,
+                rateConfirmations,
+                cntHospitalizations
+            ],
+            backgroundColor: ['#2A4E6E','yellow','black']
+        }
+        
+    ],
+  };
+
+    return (<div className='width_900'><Bar options={options} data={data} /></div>);
+  };
+  
+  export default Barchart;
